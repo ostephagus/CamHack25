7287dc2baf4f6dc245883e8db46fc9e0c8cf5383
Moth <randomcat695@protonmail.com>
2025-11-02 10:06:01 +0000

    Made plotting optional

--- MolToGrid/MolToGrid.py
+++ MolToGrid/MolToGrid.py
@@ -76,7 +76,7 @@ def best_subset_assignment(A, B, optimize_scale=True):
 
     return subset, mean_error
 
-def MolToGrid(data):
+def MolToGrid(data, plotting=False):
     # How much error we're willing to accept between the road intersections and the molecule
     acceptableError = 0.05
 
@@ -294,30 +294,31 @@ def MolToGrid(data):
     with open("MolToGrid/albBondData.txt", "w+") as f:
         f.write(str(bondData))
 
-# #Idea: Have some way to represent different elements differently?
-    for i in range(len(albPoints)):
-        coords1 = albPoints[i]
-        atom1 =inputAtoms[i]
-        nodeNum=atom1['idx']
-        for j in atom1['bonds']:
-            j=int(j)
-            if j>nodeNum:
-                atom2 = inputAtoms[j]
-                coords2 = albPoints[j]
-                plt.plot([coords1[0],coords2[0]],[coords1[1],coords2[1]],'ro-')
-    # for atom1 in inputAtoms:
-    #    nodeNum=atom1['idx']
-    #    atom1Coords = [atom1['x'],atom1['y']]
-    #    for i in atom1['bonds']:
-    #         i=int(i)
-    #         if i>nodeNum:
-    #             atom2 = inputAtoms[i]
-    #             atom2Coords = [atom2['x'],atom2['y']]
-    #             plt.plot([atom1Coords[0],atom2Coords[0]],[atom1Coords[1],atom2Coords[1]],'ro-')
+    if plotting:
+    # #Idea: Have some way to represent different elements differently?
+        for i in range(len(albPoints)):
+            coords1 = albPoints[i]
+            atom1 =inputAtoms[i]
+            nodeNum=atom1['idx']
+            for j in atom1['bonds']:
+                j=int(j)
+                if j>nodeNum:
+                    atom2 = inputAtoms[j]
+                    coords2 = albPoints[j]
+                    plt.plot([coords1[0],coords2[0]],[coords1[1],coords2[1]],'ro-')
+        # for atom1 in inputAtoms:
+        #    nodeNum=atom1['idx']
+        #    atom1Coords = [atom1['x'],atom1['y']]
+        #    for i in atom1['bonds']:
+        #         i=int(i)
+        #         if i>nodeNum:
+        #             atom2 = inputAtoms[i]
+        #             atom2Coords = [atom2['x'],atom2['y']]
+        #             plt.plot([atom1Coords[0],atom2Coords[0]],[atom1Coords[1],atom2Coords[1]],'ro-')
 
-    plt.gca().set_aspect('equal')
+        plt.gca().set_aspect('equal')
 
-    plt.show()
+        plt.show()
 
 # Need to put all coordinates in reference frame with sam's club
 # Need to handle scaling somehow. 
