389728e22693a67a2cc9bc66b4e072616b6fd8b3
ostephagus <37499766+ostephagus@users.noreply.github.com>
2025-11-02 15:02:47 +0000

    Frontend now ready for interop with wrapper_server

--- Frontend/UserInterface/PythonManager.cs
+++ Frontend/UserInterface/PythonManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace UserInterface
     {
         private readonly string scriptPath;
         private Process pythonProcess;
+        private StreamWriter pythonInput;
         public string ScriptPath => scriptPath;
 
         public PythonManager(string scriptPath)
@@ -20,12 +22,56 @@ namespace UserInterface
 
         public void StartProcess()
         {
-            throw new NotImplementedException();
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "python",
+                Arguments = scriptPath,
+                WorkingDirectory = $"{ProjectInfo.BuildInfo.SolutionDir}/..",
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = false // Change later?
+            };
+            pythonProcess = new Process { StartInfo = startInfo };
+            pythonProcess.OutputDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                    Trace.WriteLine("[PYTHON] " + e.Data);
+            };
+            pythonProcess.ErrorDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                    Trace.WriteLine("[PYTHON-ERR] " + e.Data);
+            };
+
+            pythonProcess.Start();
+
+            pythonProcess.BeginOutputReadLine();
+            pythonProcess.BeginErrorReadLine();
+
+            pythonInput = pythonProcess.StandardInput;
+        }
+
+        private void SendData(string data)
+        {
+            pythonInput.WriteLine(data);
+            pythonInput.Flush();
         }
 
         public void DrawMolecule(string moleculeName)
         {
-            throw new NotImplementedException();
+            if (pythonInput is null) throw new InvalidOperationException("Python process not started.");
+
+            SendData(moleculeName);
+        }
+
+        public void StopProcess()
+        {
+            if (pythonInput is null) return;
+            SendData("!exit");
+            pythonInput.Close();
+            pythonProcess.WaitForExit();
         }
     }
 }
