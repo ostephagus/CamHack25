0c7c01f3c0cdd5508babb4ec835206d18b4f3bb9
Marcell Perger <marcell.perger.1@gmail.com>
2025-11-02 14:12:56 +0000

    Add profiling to old wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,19 +1,23 @@
+import argparse
 import json
 
 import api_calls.compound_coords
 import linepath.linepath
 
-PLACE = 'cambridge'  # or 'cambridge'
+import cProfile
+
+PLACE = 'albuquerque'  # or 'cambridge'
 
 
 def main():
-    import argparse
-    ap = argparse.ArgumentParser()
-    ap.add_argument('name', type=str)
-    ns = ap.parse_args()
-    molJson = json.loads(api_calls.compound_coords.getJson(ns.name))
-    # MolToGrid.MolToGrid.MolToGrid(molJson)
-    linepath.linepath.run(molJson, PLACE)  # Call MolToGrid on its own
+    with cProfile.Profile() as p:
+        ap = argparse.ArgumentParser()
+        ap.add_argument('name', type=str)
+        ns = ap.parse_args()
+        molJson = json.loads(api_calls.compound_coords.getJson(ns.name))
+        # MolToGrid.MolToGrid.MolToGrid(molJson)
+        linepath.linepath.run(molJson, PLACE)  # Call MolToGrid on its own
+    p.dump_stats('p0.prof')
 
 
 if __name__=="__main__":
