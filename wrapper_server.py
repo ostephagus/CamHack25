e00ff7f1b97b3a640e6c68f3e232c875611b3a57
Marcell Perger <marcell.perger.1@gmail.com>
2025-11-02 15:03:17 +0000

    load on startup

--- wrapper_server.py
+++ wrapper_server.py
@@ -7,6 +7,7 @@ PLACE = 'albuquerque'  # or 'cambridge'
 
 
 def main():
+    linepath.linepath.load_nodes(f'{PLACE}.xml')  # discard result, just cache
     while True:
         name = input()
         if name == '!exit':
